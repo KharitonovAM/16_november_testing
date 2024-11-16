07023ddbcc05b62deff6f9cb9dd53b87ac1bf4c3
aleksandr <kharitonov_am@bk.ru>
2024-11-16 20:08:14 +0300

    make a function for long of result

--- src/calculate_daiscont.py
+++ src/calculate_daiscont.py
@@ -1,6 +1,6 @@
 from src.logic1 import calculate_taxes
 
-def calculate_discont(price:list[float], tax:float, disc:float = 0)->list[float]:
+def calculate_discont(price:list[float], tax:float, disc:float = 0, long_of_price:int = 2)->list[float]:
     actual_price = calculate_taxes(price,tax)
-    price_after_discont = [x*(1-disc/100) for x in actual_price]
+    price_after_discont = [round(x*(1-disc/100),long_of_price) for x in actual_price]
     return price_after_discont
